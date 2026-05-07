55ad524be0bb3d43b5c5c34fd14d48a9f92f0d6c
Safiraar <safira21704@gmail.com>
2026-05-07 16:56:22 +0700

    info pada prediksi

--- pages/5_Prediksi.py
+++ pages/5_Prediksi.py
@@ -39,16 +39,42 @@ def mulai_ulang():
 
 st.title("Prediksi Klaster Pelanggan")
 
-# Input user — key sama dengan nama session_state
-recency   = st.number_input("Recency (hari)",        min_value=0, key="recency")
-frequency = st.number_input("Frequency (transaksi)", min_value=0, key="frequency")
-monetary  = st.number_input("Monetary (Rp)",         min_value=0, key="monetary")
+st.markdown("""
+            Halaman ini digunakan untuk **memprediksi klaster pelanggan** dari data 
+            seorang pelanggan Transjakarta berdasarkan model RFM. 
+            Masukan data pelanggan pada kolom di bawah ini, lalu tekan tombol **prediksi**
+            untuk mengetahui klaster dari pelanggan tersebut
+            """)
+
+#Input User
+col1, col2, col3 = st.columns(3)
+with col1:
+    st.markdown("**Recency (hari)**")
+    st.caption("Jumlah hari sejak transaksi terakhir pelanggan")
+    recency = st.number_input("Recency (hari)", min_value=0, key="recency", label_visibility="collapsed")
+
+with col2:
+    st.markdown("**Frequency (transaksi)**")
+    st.caption("Total jumlah transaksi yang pernah dilakukan")
+    frequency = st.number_input("Frequency (transaksi)", min_value=0, key="frequency", label_visibility="collapsed")
+
+with col3:
+    st.markdown("**Monetary (Rp)**")
+    st.caption("Total keseluruhan uang yang telah dikeluarkan (dalam Rupiah)")
+    monetary = st.number_input("Monetary (Rp)", min_value=0, key="monetary", label_visibility="collapsed")
+
+st.divider()
+
+# # Input user — key sama dengan nama session_state
+# recency   = st.number_input("Recency (hari)",        min_value=0, key="recency")
+# frequency = st.number_input("Frequency (transaksi)", min_value=0, key="frequency")
+# monetary  = st.number_input("Monetary (Rp)",         min_value=0, key="monetary")
 
 # Tombol prediksi
 if st.session_state.hasil_prediksi is None:
     if st.button("Prediksi"):
         if frequency == 0:
-            st.error("Frequency tidak boleh 0, minimal 1")
+            st.error("⚠️ Frequency tidak boleh 0, minimal 1")
             st.stop()
 
         input_df = pd.DataFrame(
